49b34d112ea4769412bd388c2ef19fc82c757c95
CsabaDu <68659284+CsabaDu@users.noreply.github.com>
2025-02-21 17:02:42 +0100

    - Packages refreshed; - Extensions.ToTestCaseData simplified;

--- GlobalUsings.cs
+++ GlobalUsings.cs
@@ -1,5 +1,6 @@
 ﻿global using CsabaDu.DynamicTestData.DynamicDataSources;
 global using CsabaDu.DynamicTestData.NUnit.Statics;
+global using CsabaDu.DynamicTestData.Statics;
 global using CsabaDu.DynamicTestData.TestDataTypes;
 global using CsabaDu.DynamicTestData.TestDataTypes.Interfaces;
 global using NUnit.Framework;
--- Statics/Extensions.cs
+++ Statics/Extensions.cs
@@ -1,17 +1,19 @@
-﻿namespace CsabaDu.DynamicTestData.NUnit.Statics;
+﻿using static CsabaDu.DynamicTestData.DynamicDataSources.DynamicDataSource;
+
+namespace CsabaDu.DynamicTestData.NUnit.Statics;
 
 public static class Extensions
 {
     public static TestCaseData ToTestCaseData(this ITestData testData, ArgsCode argsCode, string? testMethodName = null)
     {
-        string testCase = testData.ToString()!;
-        TestCaseData testCaseData = argsCode == ArgsCode.Properties ?
+        TestCaseData testCaseData = argsCode.Defined(nameof(argsCode)) == ArgsCode.Properties ?
             new(testData.ToArgs(argsCode)[1..])
             : new(testData);
+        string testCase = testData.ToString()!;
 
         return string.IsNullOrEmpty(testMethodName) ?
             testCaseData.SetDescription(testCase)
-            : testCaseData.SetDescription(testCase).SetName(DynamicDataSource.GetDisplayName(testMethodName, [testData.TestCase])); // TODO update when nuget refreshed
+            : testCaseData.SetDescription(testCase).SetName(GetDisplayName(testMethodName, testData.TestCase));
     }
 
     public static TestCaseData ToTestCaseData<TStruct>(this ITestDataReturns<TStruct> testData, ArgsCode argsCode, string? testMethodName = null) where TStruct : struct
